37797f24207f6bebf273804f938d8ab315255bbf
yu-jinny <151537809+yu-jinny@users.noreply.github.com>
2024-01-03 20:03:46 +0900

    [ios개발입문}카운터앱

--- counterApp/ViewController.swift
+++ counterApp/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var countLabel: UILabel!
+    var count:Int = 0
+    
+    @IBOutlet weak var increaseButton: UIButton!
+    @IBOutlet weak var reduceButton: UIButton!
 
+    
+    @IBAction func touchedReduceButton(_ sender: Any) {
+        self.count -= 1
+        self.refreshCountLabel()
+    }
+    
+    @IBAction func touchedIncreaseButton(_ sender: Any) {
+        self.count += 1
+        self.refreshCountLabel()
+    }
+    private func refreshCountLabel(){
+        self.countLabel.text = String(self.count)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.refreshCountLabel()
+        
     }
-
-
 }
-
